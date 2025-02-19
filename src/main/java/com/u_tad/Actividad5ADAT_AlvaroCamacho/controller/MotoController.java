4684f6fe2f04b52e0a5df99b6e072df49d24e118
Alvaro Camacho <accc1997@hotmail.com>
2025-02-19 19:42:25 +0100

    Commit definitivo

--- src/main/java/com/u_tad/Actividad5ADAT_AlvaroCamacho/controller/MotoController.java
+++ src/main/java/com/u_tad/Actividad5ADAT_AlvaroCamacho/controller/MotoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/v1/motos")
@@ -23,8 +24,7 @@ public class MotoController {
     @PostMapping
     public ResponseEntity<Moto> save(@RequestBody Moto moto){
         Moto newMoto = motoService.saveOrUpdateBike(moto);
-        //return ResponseEntity.status(HttpStatus.ACCEPTED).body(newMoto);
-        return ResponseEntity.ok(newMoto);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(newMoto);
     }
 
     @GetMapping
@@ -33,8 +33,35 @@ public class MotoController {
         return ResponseEntity.ok(allMotos);
     }
 
-    private void hola() {
-        System.out.println("Hola");
+    @GetMapping("/{idMoto}")
+    public ResponseEntity<Optional<Moto>> getById(@PathVariable int idMoto) {
+        Optional<Moto> moto = motoService.getBikeById(idMoto);
+        if (moto.isPresent()) {
+            return ResponseEntity.ok(moto);
+        }
+        return ResponseEntity.notFound().build();
     }
 
+    @GetMapping("/searchBrand")
+    public ResponseEntity<List<Moto>> getByBrand(@RequestParam String brand) {
+        List<Moto> motosMarcas = motoService.getBikesByBrand(brand);
+        if (motosMarcas.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(motosMarcas);
+    }
+
+    @DeleteMapping("/{idMoto}")
+    public void deleteBike(@PathVariable int idMoto) {
+        motoService.deleteBike(idMoto);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Moto>> getByBudget(@RequestParam int price){
+        List<Moto> affordableBikes = motoService.getBikesByBudget(price);
+        if (affordableBikes.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(affordableBikes);
+    }
 }
--- src/main/java/com/u_tad/Actividad5ADAT_AlvaroCamacho/model/Moto.java
+++ src/main/java/com/u_tad/Actividad5ADAT_AlvaroCamacho/model/Moto.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
+//@Setter
+//@Getter
 @Data
 @Entity
 @Table(name= "motos")
@@ -16,6 +16,79 @@ public class Moto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String brand;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(int displacement) {
+        this.displacement = displacement;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public double getTorque() {
+        return torque;
+    }
+
+    public void setTorque(double torque) {
+        this.torque = torque;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     private String model;
     private int displacement;
     private double power;
